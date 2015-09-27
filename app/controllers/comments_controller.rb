3096f357f0e3dd85d8007b0ef8de888baf5df103
Akshay <akshay@ubuntu.ubuntu-domain>
2015-09-27 23:19:36 +0530

    article comments fixation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,6 +14,17 @@ class CommentsController < ApplicationController
   end
 
   def create
+    params[:comment_parent].each {|key,value| @commentable = key.constantize.find(value)}
     debugger
+    @comment = @commentable.comments.new(comment_params)
+    if @comment.save
+      redirect_to :back
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
